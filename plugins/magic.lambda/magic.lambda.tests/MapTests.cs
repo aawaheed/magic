415610076d7756106122c1db9ce9cec8987019da
polterguy <thomas@ainiro.io>
2026-03-04 15:52:12 +0200

    b

---
+++ plugins/magic.lambda/magic.lambda.tests/MapTests.cs
@@ -0,0 +1,61 @@
+/*
+ * Magic Cloud, copyright (c) 2023 Thomas Hansen. See the attached LICENSE file for details. For license inquiries you can send an email to thomas@ainiro.io
+ */
+
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using magic.node.extensions;
+
+namespace magic.lambda.tests
+{
+    public class MapTests
+    {
+        [Fact]
+        public void Map_01()
+        {
+            var lambda = Common.Evaluate(@"
+.src
+   .:int:1
+   .:int:2
+   .:int:3
+map:x:@.src/*
+   math.increment:x:@.dp/#
+   return:x:-");
+
+            var map = lambda.Children.First(x => x.Name == "map");
+            Assert.Equal(3, map.Children.Count());
+            Assert.Equal(2, map.Children.First().GetEx<int>());
+            Assert.Equal(3, map.Children.Skip(1).First().GetEx<int>());
+            Assert.Equal(4, map.Children.Skip(2).First().GetEx<int>());
+        }
+
+        [Fact]
+        public async Task MapAsync_01()
+        {
+            var lambda = await Common.EvaluateAsync(@"
+.src
+   .:int:1
+   .:int:2
+   .:int:3
+map:x:@.src/*
+   math.increment:x:@.dp/#
+   return:x:-");
+
+            var map = lambda.Children.First(x => x.Name == "map");
+            Assert.Equal(3, map.Children.Count());
+        }
+
+        [Fact]
+        public void Map_ThrowsWithoutReturn()
+        {
+            Assert.Throws<HyperlambdaException>(() => Common.Evaluate(@"
+.src
+   .:int:1
+map:x:@.src/*
+   mt
+      get-value:x:@.dp/#
+      .:int:0"));
+        }
+    }
+}
---
+++ plugins/magic.lambda/magic.lambda/loops/Map.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * Magic Cloud, copyright (c) 2023 Thomas Hansen. See the attached LICENSE file for details. For license inquiries you can send an email to thomas@ainiro.io
+ */
+
+using System.Linq;
+using System.Collections.Generic;
+using magic.node;
+using magic.node.extensions;
+using magic.signals.contracts;
+
+namespace magic.lambda.loops
+{
+    /// <summary>
+    /// [map] slot allowing you to map a list of nodes into a new list, resulting from the evaluation of an expression.
+    /// </summary>
+    [Slot(Name = "map")]
+    public class Map : ISlot
+    {
+        /// <summary>
+        /// Implementation of signal.
+        /// </summary>
+        /// <param name="signaler">Signaler used to signal.</param>
+        /// <param name="input">Parameters passed from signaler.</param>
+        public void Signal(ISignaler signaler, Node input)
+        {
+            var mapped = new List<Node>();
+
+            // Iterating through each source node.
+            foreach (var idxSource in input.Evaluate().ToList())
+            {
+                // Making sure we can reset back to original nodes after every single iteration.
+                var old = input.Children.Select(x => x.Clone()).ToList();
+
+                // Making sure we're able to handle returned values and nodes from slot implementation.
+                var result = new Node();
+                signaler.Scope("slots.result", result, () =>
+                {
+                    input.Insert(0, new Node(".dp", idxSource));
+
+                    // Evaluating mapper lambda.
+                    signaler.Signal("eval", input);
+
+                    // Resetting back to original nodes.
+                    input.Clear();
+                    input.AddRange(old.Select(x => x.Clone()));
+                });
+
+                AppendMappedResult(mapped, result);
+            }
+
+            // Replacing input with mapped result.
+            input.Value = null;
+            input.Clear();
+            input.AddRange(mapped);
+        }
+
+        static void AppendMappedResult(List<Node> mapped, Node result)
+        {
+            if (result.Value == null && !result.Children.Any())
+                throw new HyperlambdaException("[map] requires [return] to produce a value or nodes");
+
+            if (result.Value != null)
+                mapped.Add(new Node(".", result.Value));
+
+            if (result.Children.Any())
+                mapped.AddRange(result.Children.Select(x => x.Clone()));
+        }
+    }
+}
