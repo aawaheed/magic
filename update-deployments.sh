6798482d661c150ba01f77efad79b411e6b67732
polterguy <thomas@ainiro.io>
2026-04-04 10:11:35 +0300

    Update update-deployments.sh

--- update-deployments.sh
+++ update-deployments.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 NAMESPACE="cloudlets"
-IMAGE="servergardens/magic-backend:v22.7.23"
+IMAGE="servergardens/magic-backend:v22.8.0"
 FSGROUP="999"
 
 echo "========================================="
